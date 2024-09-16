8a9203e7df1ab6348927de421a32bb48a9ca1e84
Nicholas McCracken <nicholas.r.mccracken@gmail.com>
2024-09-16 01:39:21 -0400

    Reimplement rules as CLI class

---
+++ lib/cli.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+class CLI
+  # start menu
+  # first thing to run in the program
+  # prints out welcome message to user and recieves input for which mode
+  # the user wishes to play
+  def start
+    # output the opening message
+    intro = File.read('resources/intro.txt')
+    puts intro
+
+    choice = 1
+    while choice != 0
+      # give and recieve game option
+      options = File.read('resources/options.txt')
+      puts options
+
+      # cut input into integer
+      input = gets.chomp
+      choice = input.to_i
+
+      # game cases
+      case choice
+      # exit condition
+      when 0
+        puts "Its been fun seeing you !!\n"
+      # single player mode
+      when 1
+        single_player
+      # 2 player mode
+      when 2
+        two_player
+      # rules mode
+      when 3
+        rules
+      # programmer notes
+      when 4
+        puts 'notes to come'
+      # invalid input condition
+      else
+        puts "error: #{choice} is a non valid input, try again"
+      end
+
+    end
+  end
+
+  # Defines the rules of the game
+  def rules
+    exit = 0
+    # exit loop when user is done reading rules
+    while exit != 1
+      # output and read options for rules explaination
+      puts '---------------------------------------------------------------------------'
+      puts "What would you like to further explore\n1: display\n2: how to read features\n3: how to make a set\n0: back to menu\n"
+      input = gets.chomp
+      option = input.to_i
+
+      case option
+      # case for display
+      when 1
+        puts 'the format of the dispay is as follows:'
+        puts "% 1 2 3 4\n0 _ _ _ _\n1 _ # _ _\n2 _ _ _ _"
+        puts 'the users will adress each card as follows'
+        puts 'to index # user inputs location (1, 2)'
+      # cae for features
+      when 2
+        puts 'features are displayed as follows'
+        puts '[NumberColor-ShapeShade]'
+        puts "Number: 1 2 or 3, Color: r g or b, Shape: oval sqiggle diamond,\nShade: none, partial, full"
+      # case for set
+      when 3
+        puts 'Set = 3 cards whose feature is EITHER the same or DIFFERENT on each card'
+        puts "this means any feature in the set is either\nCommon to all 3\nor Different in all 3"
+      # exit condition
+      when 0
+        exit = 1
+      # invalid input condition
+      else
+        puts "Error, input of #{option} is not a choice\n"
+      end
+    end
+  end
+end
